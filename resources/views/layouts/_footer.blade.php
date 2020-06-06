341ed4b16f4745c2aaed64a6f18b20439979606c
luban <luban@lubancafe.com>
2020-06-06 23:37:36 +0800

    base layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+        由 <a href="https://swotin.com" target="_blank">Team</a> Design&Code <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="float-right"><a href="mailto:name@email.com">Contact us</a></p>
+    </div>
+</footer>
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title', 'Root')
+
+@section('content')
+  <h1>This is root page</h1>
+@stop
