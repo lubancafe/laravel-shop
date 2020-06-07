a4169b5dc748f311d7bc3b93d1fa883f3d58178e
luban <luban@lubancafe.com>
2020-06-07 17:02:07 +0800

    admin - users list

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+use Encore\Admin\Traits\DefaultDatetimeFormat;
+
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use Notifiable;
+    use Notifiable, DefaultDatetimeFormat;
 
     /**
      * The attributes that are mass assignable.
