0d7786176b59c478ad7f306fb1062c9306b54a84
luban <luban@lubancafe.com>
2020-06-08 21:52:00 +0800

    Exception Handling

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Product;
+use Illuminate\Http\Request;
+use App\Exceptions\InvalidRequestException;
 
 class ProductsController extends Controller
 {
@@ -54,7 +55,7 @@ class ProductsController extends Controller
     {
         // 判断商品是否已经上架，如果没有上架则抛出异常。
         if (!$product->on_sale) {
-            throw new \Exception('商品未上架');
+            throw new InvalidRequestException('商品未上架');
         }
 
         return view('products.show', ['product' => $product]);
