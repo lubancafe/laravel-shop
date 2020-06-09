fa9e3d3e0c262f8caab78831053f7ce1f2a33585
Tim Liu <liut@lesconcierges.com>
2020-06-09 17:50:49 +0800

    Canvas and Order Models

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,28 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
 });
 
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+
+Route::namespace('Studio')->prefix(config('studio.path'))->group(function () {
+    Route::prefix('api')->group(function () {
+        Route::prefix('posts')->group(function () {
+            Route::get('/', 'PostController@index');
+            Route::get('{identifier}/{slug}', 'PostController@show')->middleware('Canvas\Http\Middleware\Session');
+        });
+
+        Route::prefix('tags')->group(function () {
+            Route::get('/', 'TagController@index');
+            Route::get('{slug}', 'TagController@show');
+        });
+
+        Route::prefix('topics')->group(function () {
+            Route::get('/', 'TopicController@index');
+            Route::get('{slug}', 'TopicController@show');
+        });
+
+        Route::prefix('users')->group(function () {
+            Route::get('{identifier}', 'UserController@show');
+        });
+    });
+
+    Route::get('/{view?}', 'ViewController')->where('view', '(.*)')->name('studio');
+});
