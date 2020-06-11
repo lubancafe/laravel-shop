f24ffe599166e7f61fde0d1617788ebda446c56a
Luban <luban@lubancafe.com>
2020-06-11 11:04:33 +0800

    Admin - ship

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -22,4 +22,5 @@ Route::group([
 //    $router->resource('orders', OrdersController::class);   
     $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
     $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show');
+    $router->post('orders/{order}/ship', 'OrdersController@ship')->name('admin.orders.ship');
 });
