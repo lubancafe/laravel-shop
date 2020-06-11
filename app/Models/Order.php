4b412c1b1c0d959bc5d70df7bff48817a2569d37
Luban <luban@lubancafe.com>
2020-06-11 14:25:41 +0800

    Admin - Coupon List

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -113,5 +113,10 @@ class Order extends Model
         } while (self::query()->where('refund_no', $no)->exists());
 
         return $no;
+    }   
+    
+    public function couponCode()
+    {
+        return $this->belongsTo(CouponCode::class);
     }    
 }
