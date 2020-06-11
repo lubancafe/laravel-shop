06333b605619d7386c9f4029d0581c7d6bbfbdab
Luban <luban@lubancafe.com>
2020-06-11 11:45:45 +0800

    Storefront: Confirm received

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use App\Services\OrderService;
 
 use App\Http\Requests\OrderRequest;
+use App\Exceptions\InvalidRequestException;
 //use App\Exceptions\InvalidRequestException;
 
 class OrdersController extends Controller
@@ -40,4 +41,22 @@ class OrdersController extends Controller
 
         return $orderService->store($user, $address, $request->input('remark'), $request->input('items'));
     }
+
+    public function received(Order $order, Request $request)
+    {
+        // 校验权限
+        $this->authorize('own', $order);
+
+        // 判断订单的发货状态是否为已发货
+        if ($order->ship_status !== Order::SHIP_STATUS_DELIVERED) {
+            throw new InvalidRequestException('发货状态不正确');
+        }
+
+        // 更新发货状态为已收到
+        $order->update(['ship_status' => Order::SHIP_STATUS_RECEIVED]);
+
+        // 返回原页面
+        //return redirect()->back();
+        return $order;
+    }    
 }
